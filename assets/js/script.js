001b1e699ee428b158aff4fbdc003ee13b844003
antoineThomasCode <73194840+antoineThomasCode@users.noreply.github.com>
2021-10-21 15:36:30 +0200

    changed design header and call to action

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const buttonOne = document.getElementById("boutique");
+
+function displayFalg(){
+       console.log("Button is running");
+       console.log("Button is running"); console.log("Button is running"); console.log("Button is running"); console.log("Button is running"); console.log("Button is running"); console.log("Button is running"); console.log("Button is running"); console.log("Button is running"); console.log("Button is running");
+    
+};
+
+buttonOne.onclick = doSomething();
