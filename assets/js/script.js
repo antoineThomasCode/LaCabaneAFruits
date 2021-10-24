d645d47360c5bb5f4d83a3a2716f6dac218d8282
antoineThomasCode <73194840+antoineThomasCode@users.noreply.github.com>
2021-10-25 00:43:41 +0200

    added button style

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,5 +6,6 @@ var btnEngagement = document.getElementById('btn_engagement');
 btnEngagement.addEventListener('click', function(e){
        var containerEngagement = document.getElementById('engagement_container');
               containerEngagement.style.display = "flex";
+              btnEngagement.style.display = "none";
 });
 console.log(btnEngagement);
